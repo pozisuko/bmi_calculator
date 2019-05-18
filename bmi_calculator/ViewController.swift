f39c04fd82d27f86f61737b540f0b4968fbf43d7
chiaki-kato <makuragi@me.com>
2019-05-18 11:33:52 +0900

    created xcode project

---
+++ bmi_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bmi_calculator
+//
+//  Created by 加藤千晶 on 2019/05/18.
+//  Copyright © 2019 pozisuko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
